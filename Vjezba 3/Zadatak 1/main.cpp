dc16ae0b55f149b49ca186c4d3faf90c32582a2b
Amar.B <amaron_96@live.com>
2017-03-26 18:20:28 +0200

    Dodao vjezbu 3 (nepotpunu)

---
+++ Vjezba 3/Zadatak 1/main.cpp
@@ -0,0 +1,50 @@
+#include "mbed.h"
+#include "N5110.h"
+#include <string>
+#define dp23 P0_0
+
+const float VCC = 3.3;
+const float R = 5;
+ 
+//N5110 lcd(VCC,SCE,RST,DC,MOSI,SCLK,LED));
+N5110 lcd(dp4,dp24,dp23,dp25,dp2,dp6,dp18);
+
+AnalogIn voltage(dp9);
+
+DigitalOut enable(dp14);
+DigitalIn taster(dp1);
+
+bool ocitaj_taster(bool &ugasen) {
+    if(ugasen && taster) {
+        ugasen = false;
+        return true;
+    } else if(!taster)
+        ugasen = true; 
+    return false;
+}
+
+float otpor(float U) {
+    return U * R / VCC;
+}
+
+int main() {
+    bool resistance_mode = false, ugasen = true;
+    //deaktivacija led dioda
+    enable=1;
+    
+    //inicijalizacija displeja
+    lcd.init();
+    
+    while(1) {
+        char poruka[50];
+        if(ocitaj_taster(ugasen))
+            resistance_mode = 1 - resistance_mode;
+        lcd.clear();
+        if(resistance_mode) 
+            sprintf(poruka, "Otpor: %f Ohm", otpor(voltage.read()* 20000));
+        else 
+            sprintf(poruka, "Napon: %f V", voltage.read() * 3.3);
+        lcd.printString(poruka, 0, 0);
+        wait(0.01);
+    }
+}
